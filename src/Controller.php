6236e0584bda22801a6c762b0b263cfce40b207d
Jagepard <dankorot@gmail.com>
2016-09-26 14:44:34 +0300

    Добавил геттеры сеттеры для Model

--- src/Controller.php
+++ src/Controller.php
@@ -309,4 +309,20 @@ class Controller
         return $this->twig;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param mixed $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
 }
