42c84a04620ee663afbdb6b7c15949fc08b9b953
Jagepard <dankorot@gmail.com>
2017-03-30 10:17:28 +0300

    Добавляет константу BP

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
-require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+define('BP', dirname(dirname(__DIR__)) . '/');
+
+require_once BP . 'vendor/autoload.php';
 require_once dirname(__DIR__) . '/IController.php';
 require_once dirname(__DIR__) . '/Controller.php';
