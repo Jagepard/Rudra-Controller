d46315da51f5750a9f9359382a7d2b8903a93e91
Jagepard <dankorot@gmail.com>
2020-09-10 17:45:48 +0300

    moved and update

---
+++ src/ControllerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author    : Jagepard <jagepard@yandex.ru">
+ * @license   https://mit-license.org/ MIT
+ */
+
+namespace Rudra\Controller;
+
+interface ControllerInterface
+{
+    public function init();
+    public function before(); // The method is executed before calling the controller
+    public function after(); // The method is executed after calling the controller
+//    public function template(array $config): void;
+    public function csrfProtection(): void;
+    public function view(string $path, array $data = []): string;
+    public function render(string $path, array $data = []): void;
+}
