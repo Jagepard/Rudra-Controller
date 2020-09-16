35cdbdccf7b8dbb69a2894d12fc81e64ac5688a4
Jagepard <dankorot@gmail.com>
2020-09-16 18:21:41 +0300

    cut view

--- src/ControllerInterface.php
+++ src/ControllerInterface.php
@@ -12,8 +12,5 @@ interface ControllerInterface
     public function init();
     public function before(); // The method is executed before calling the controller
     public function after(); // The method is executed after calling the controller
-    public function template(array $config): void;
     public function csrfProtection(): void;
-    public function view(string $path, array $data = []): string;
-    public function render(string $path, array $data = []);
 }
