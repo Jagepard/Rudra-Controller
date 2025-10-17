f6b0645436e49d8c823ebaadde29654a52080fed
Jagepard <jagepard@yandex.ru>
2025-10-17 12:00:38 +0300

    add Session::set

--- src/Controller.php
+++ src/Controller.php
@@ -63,6 +63,6 @@ class Controller implements ControllerInterface
             $csrf[] = bin2hex(random_bytes(32));
         }
 
-        Session::set(["csrf_token", $csrf]);
+        Session::set("csrf_token", $csrf);
     }
 }
